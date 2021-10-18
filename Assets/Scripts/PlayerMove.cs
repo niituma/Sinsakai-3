08320bb358bdc4cc717f3a02e1b31709ef5eba17
sakezuma2 <sakezuma@gmail.com>
2021-10-19 03:14:20 +0900

    player Character Controller化にした

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,14 +10,16 @@ public class PlayerMove : MonoBehaviour
     float h, v;
     float _animationspeed;
     Rigidbody _rb = default;
+    CharacterController _controller;
     Animator _anim = default;
     /// <summary>入力された方向の XZ 平面でのベクトル</summary>
-    Vector3 _dir;
+    Vector3 _dir = Vector3.zero;
     public Vector2 move;
 
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        //_rb = GetComponent<Rigidbody>();
+        _controller = GetComponent<CharacterController>();
         _anim = GetComponent<Animator>();
     }
 
@@ -26,23 +28,20 @@ public class PlayerMove : MonoBehaviour
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
         move = new Vector2(h, v);
-        _dir = Vector3.forward * v + Vector3.right * h;
+        _dir = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         // カメラのローカル座標系を基準に dir を変換する
         _dir = Camera.main.transform.TransformDirection(_dir);
+        _dir = _dir * _movePower;
         // カメラは斜め下に向いているので、Y 軸の値を 0 にして「XZ 平面上のベクトル」にする
-        _dir.y = 0;
+        //_dir.y = 0;
         // キャラクターを「現在の（XZ 平面上の）進行方向」に向ける
-        Vector3 forward = _rb.velocity;
+        Vector3 forward = _controller.velocity;
         forward.y = 0;
 
         if (forward != Vector3.zero)
         {
             this.transform.forward = forward;
         }
-
-        Vector3 velo = new Vector3(_rb.velocity.x, _rb.velocity.y, _rb.velocity.x);
-        velo.y = _rb.velocity.y;
-        _rb.velocity = velo;
 
         Jump();
 
@@ -65,17 +64,17 @@ public class PlayerMove : MonoBehaviour
     void FixedUpdate()
     {
         // 「力を加える」処理は力学的処理なので FixedUpdate で行うこと
-        _rb.AddForce(_dir.normalized * _movePower, ForceMode.Impulse);
+        _controller.Move(_dir.normalized * Time.deltaTime);
     }
 
     void Jump()
     {
-        Vector3 velosity = _rb.velocity;
+        Vector3 velosity = _controller.velocity;
         if (Input.GetButtonDown("Jump"))
-            _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
+            _dir.y = _jumpPower;
         else if (!Input.GetButtonDown("Jump") && velosity.y > 0)
-            velosity.y *= _gravityPower;
+            _dir.y = _dir.y - (_gravityPower * Time.deltaTime);
 
-        _rb.velocity = velosity;
+        _controller.Move(velosity);
     }
 }
