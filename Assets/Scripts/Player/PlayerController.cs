715aaf8f6eca3aeb9b60008a6eeaca1803457826
sakezuma2 <sakezuma@gmail.com>
2021-12-27 03:55:49 +0900

    playercontriller修正

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -121,7 +121,7 @@ public class PlayerController : MonoBehaviour
             _anim.CrossFade("Braced Jump From Wall", 0.2f);
             _stopmovedir = true;
             _isclimd = false;
-        }else if (Input.GetKeyDown(KeyCode.C) && _isclimd && _input.move != Vector2.zero)
+        }else if (Input.GetKeyDown(KeyCode.C) && !_isclimd && _input.move != Vector2.zero)
         {
             _isSkillDash = true;
         }
@@ -136,10 +136,6 @@ public class PlayerController : MonoBehaviour
         }
 
         _isjump = _input.jump;
-        if (_isjump)
-        {
-            Debug.Log("Aa");
-        }
         TargetLookOn();
         Climb();
         Jump();
