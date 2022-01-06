783aa160a79fc43368d1da7d03590ea3874c5b48
sakezuma2 <sakezuma@gmail.com>
2022-01-06 19:06:00 +0900

    氷の魔法のプログラム書き途中

--- Assets/Scripts/ApproachTarget.cs
+++ Assets/Scripts/ApproachTarget.cs
@@ -6,9 +6,13 @@ using System.Linq;
 public class ApproachTarget : MonoBehaviour
 {
     [SerializeField] float _lifetime = 3f;
-    [SerializeField] float _movespeed = 3f;
-    [SerializeField] float _lookonspeed = 5f;
+    [SerializeField] float _moveSpeed = 3f;
+    [SerializeField] float _upspeed = 3f;
+    [SerializeField] float _lookonSpeed = 5f;
     [SerializeField] float _moveDistance;
+    [SerializeField] Vector3 _gettargetsRangeCenter = default;
+    /// <summary>敵のターゲットロックできる範囲の半径</summary>
+    [SerializeField] float _targetsRangeRadius = 1f;
     [SerializeField] GameObject _hitEff = default;
     [SerializeField] GameObject _createIce = default;
     GameObject[] _targets = default;
@@ -18,32 +22,58 @@ public class ApproachTarget : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody>();
         _targets = GameObject.FindGameObjectsWithTag("Enemy");
-        _rb.velocity = this.transform.forward * _movespeed;
+        _rb.velocity = this.transform.forward * _moveSpeed;
         Destroy(gameObject, _lifetime);
     }
     void Update()
     {
-        GameObject _neartarget = _targets?.OrderBy(t => Vector3.Distance(t.transform.position, this.transform.position)).FirstOrDefault();
+        _rb.AddForce(Vector3.up * _upspeed);
 
-        _rb.AddForce(Vector3.up * _lookonspeed);
-
-        if (_neartarget != null)
+        var hit = Physics.OverlapSphere(GetTargetsRangeCenter(), _targetsRangeRadius).ToList();
+        if (hit != null)
         {
-            Vector3 targetPos = _neartarget.transform.position;
-
-            targetPos.y = transform.position.y;
-            // オブジェクトを変数 targetPos の座標方向に向かせる
-            transform.LookAt(targetPos);
-
-            float distance = Vector3.Distance(transform.position, _neartarget.transform.position);
-            // オブジェクトとターゲットオブジェクトの距離判定
-            // 変数 distance（ターゲットオブジェクトとオブジェクトの距離）が変数 moveDistance の値より小さければ
-            if (distance <= _moveDistance)
+            foreach (var c in hit)
             {
-                // 変数 lookonSpeed を乗算した速度でオブジェクトを移動させる
-                this.transform.position = Vector3.MoveTowards(this.transform.position, targetPos, _lookonspeed * Time.deltaTime);
+                if (c.gameObject.tag == "Enemy")
+                {
+                    c.GetComponent<Rigidbody>().isKinematic = true;
+                    c.transform.position = Vector3.MoveTowards(c.transform.position, transform.position, _lookonSpeed * Time.deltaTime);
+                }
             }
         }
+
+        //GameObject _neartarget = _targets?.OrderBy(t => Vector3.Distance(t.transform.position, this.transform.position)).FirstOrDefault();
+
+        //if (_neartarget != null)
+        //{
+        //    Vector3 targetPos = _neartarget.transform.position;
+
+        //    targetPos.y = transform.position.y;
+        //    // オブジェクトを変数 targetPos の座標方向に向かせる
+        //    transform.LookAt(targetPos);
+
+        //    float distance = Vector3.Distance(transform.position, _neartarget.transform.position);
+        //    // オブジェクトとターゲットオブジェクトの距離判定
+        //    // 変数 distance（ターゲットオブジェクトとオブジェクトの距離）が変数 moveDistance の値より小さければ
+        //    if (distance <= _moveDistance)
+        //    {
+        //        // 変数 lookonSpeed を乗算した速度でオブジェクトを移動させる
+        //        this.transform.position = Vector3.MoveTowards(this.transform.position, targetPos, _lookonspeed * Time.deltaTime);
+        //    }
+        //}
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(GetTargetsRangeCenter(), _targetsRangeRadius);
+    }
+    Vector3 GetTargetsRangeCenter()
+    {
+        Vector3 center = this.transform.position + this.transform.forward * _gettargetsRangeCenter.z
+            + this.transform.up * _gettargetsRangeCenter.y
+            + this.transform.right * _gettargetsRangeCenter.x;
+        return center;
     }
     private void OnDestroy()
     {
@@ -51,13 +81,11 @@ public class ApproachTarget : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Enemy")
+        if (other.tag != "Enemy")
         {
-            Instantiate(_hitEff,transform.position,Quaternion.identity);
+            Instantiate(_hitEff, transform.position, Quaternion.identity);
             Instantiate(_createIce, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
-
-            Destroy(this.gameObject);
     }
 }
---
+++ Assets/Scripts/EnemyIskinematicOff.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIskinematicOff : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
