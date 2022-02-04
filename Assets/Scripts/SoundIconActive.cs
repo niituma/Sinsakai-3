e7af2fdb9d82ddfe4f6e6a4f5add9cafd11a62bf
sakezuma2 <sakezuma@gmail.com>
2022-02-05 02:47:25 +0900

    fadeoutin入れた

---
+++ Assets/Scripts/SoundIconActive.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundIconActive : MonoBehaviour
+{
+    [SerializeField] GameObject SoundIcon = default;
+    [SerializeField] GameObject MuteIcon = default;
+    [SerializeField] Slider _slider;
+    [SerializeField] AudioSource BGM;
+
+    public void ChangeVolume()
+    {
+        BGM.volume = _slider.value;
+
+        if (_slider.value > 0)
+        {
+            SoundIcon.SetActive(true);
+            MuteIcon.SetActive(false);
+        }
+        else
+        {
+            SoundIcon.SetActive(false);
+            MuteIcon.SetActive(true);
+        }
+    }
+}
