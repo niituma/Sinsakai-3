2627968d12167102f67ef89746286df132be6691
sakezuma <sakezuma@gmail.com>
2022-02-24 19:21:35 +0900

    push test

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -19,7 +19,6 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         _audioBGM.clip = BGM;
         _audioBGM.Play();
     }
